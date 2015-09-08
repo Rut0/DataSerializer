f37e3ace1f6eb49b2029a791c4e1c66082a21929
Winston <winstonwut@gmail.com>
2015-09-08 18:08:50 -0400

    Initial Commit

---
+++ DataSerializer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
