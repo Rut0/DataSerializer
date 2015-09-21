d6c7a27c30c041a9e8f0623dfc9004c459ecfd08
Winston <winstonwut@gmail.com>
2015-09-21 13:11:53 -0400

    .

---
+++ DataSerializer/DataSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSerializer
+{
+    public class DataSerializer
+    {
+        public static byte[] Serialize(object data, bool compress)
+        {
+            if (IsPrimitive(data))
+            {
+                //Serialize(data);
+            }
+        }
+
+        public static string Serialize(object data, bool compress, bool base64)
+        {
+            return Convert.ToBase64String(Serialize(data, compress));
+        }
+
+        private static bool IsPrimitive(object data)
+        {
+            return data.GetType().Namespace == "System";
+        }
+    }
+}
